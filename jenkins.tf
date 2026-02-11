175f7c9c6c9251cb9ca661d3ecf8a9883ce84724
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-11 23:31:00 +0530

    WIP: practicing changes on personal PC

--- jenkins.tf
+++ jenkins.tf
@@ -1,31 +1,33 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.28.0"
     }
   }
 }
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "jenkins" {
-    ami           = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.small"
-    vpc_security_group_ids = ["sg-0bbdd2b154434fbfd"] 
-    user_data = file("server.sh")
-    tags = {
-        Name = "jenkins-server"
-    }
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = "sg-0bbdd2b154434fbfd"
+  subnet_id              = ["subnet-00d8b90d93d5ad88f"]
+  user_data              = file("server.sh")
+  tags = {
+    Name = "jenkins-server"
+  }
 }
 
 resource "aws_instance" "jenkins-agent" {
-    ami           = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.medium"
-    vpc_security_group_ids = ["sg-0bbdd2b154434fbfd"]
-    user_data = file("agent.sh")
-    tags = {
-        Name = "jenkins-agent"
-    }
+  ami                    = "ami-0220d79f3f480ecf5"
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = "sg-0bbdd2b154434fbfd"
+  subnet_id              = ["subnet-00d8b90d93d5ad88f"]
+  user_data              = file("agent.sh")
+  tags = {
+    Name = "jenkins-agent"
+  }
 }
