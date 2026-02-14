cbb8bc1f501caddd57d58193e5f0477c331efbe3
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-14 06:19:08 +0530

    updated agent.sh

--- agent.sh
+++ agent.sh
@@ -8,3 +8,11 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 dnf install zip unzip -y
+cat <<EOT>> /tmp/resize.sh
+#!/bin/bash
+sudo growpart /dev/nvme0n1 4
+sudo pvresize /dev/nvme0n1p4
+sudo lvextend -L +8G /dev/RootVG/homeVol
+sudo xfs_growfs /home
+
+EOT
